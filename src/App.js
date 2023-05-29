d8e78197908d6a03a7958aebe2c043de21b14521
FatemehAria <ftemewh@gmail.com>
2023-05-29 19:15:42 +0330

    user completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import axios from "axios";
 import "./App.css";
 import { useEffect } from "react";
 import Users from "./Users";
+import User from "./User";
 
 function App() {
-  return <div className="App">
-    {/* <Users /> */}
-    
-  </div>;
+  return (
+    <div className="App">
+      {/* <Users /> */}
+      <User />
+    </div>
+  );
 }
 
 export default App;
--- src/User.js
+++ src/User.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { gql, useQuery } from "@apollo/client";
 
 const GET_USER = gql`
-  query {
-    user(id: 1) {
+  query getUser($id: ID!) {
+    user(id: $id) {
       id
       name
       username
@@ -12,7 +12,37 @@ const GET_USER = gql`
   }
 `;
 function User() {
-  return <div></div>;
+  const [id, setId] = useState("");
+  const { loading, data, error } = useQuery(GET_USER, {
+    variables: { id: id },
+  });
+  //   console.log({ loading, data, error });
+  return (
+    <div>
+      <input
+        placeholder="Enter user ID"
+        onChange={(e) => setId(e.target.value)}
+      />
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error.message}</p>
+      ) : (
+        <div style={{paddingTop:"1rem"}}>
+          {Object.values(data).map((item) => {
+            return (
+              <div key={item.id}>
+                <span>{item.id}-</span>
+                <span>{item.name}-</span>
+                <span>{item.username}</span>
+                <p>{item.phone}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default User;
